38e2cc9966b2e0d6c33931def178fe824b2a2323
Rahma <154680207+raahmajourney@users.noreply.github.com>
2025-04-15 08:21:08 +0800

    memperbaiki navbar dan mengganti blog menjadi post

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-<x-slot:title>{{ $title }}</x-slot:title>
-  <h3>Ini halaman home blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,20 @@ Route::get('/home', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page'] );
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rahma',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus excepturi doloremque, nihil eos accusantium tempore error quos repudiandae, amet asperiores ipsa neque dolorum in vel temporibus dolore? Rem, eaque. Vero.'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rahma',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, enim accusantium voluptatibus, explicabo quaerat iste excepturi, distinctio earum fugiat consequatur harum ex dolor animi? Adipisci quam impedit corporis debitis iusto!'
+        ]
+
+    ] ] );
 });
 
 Route::get('/about', function () {
